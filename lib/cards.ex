aa547ebcfd88f4bc08381f93e9d7b3322cd58f7e
reinoptland <reinoptland@gmail.com>
2017-09-25 22:08:19 +0200

    Add some answers from the tutorial

--- lib/cards.ex
+++ lib/cards.ex
@@ -21,8 +21,19 @@ defmodule Cards do
     ["Ace", "Two", "Three"]
   end
 
+  # My solution
   def shuffle(deck) do
     Enum.take_random(deck, length(deck))
+  end
+
+  # According to tutorial (duh!)
+  # def shuffle(deck) do
+  #   Enum.shuffle(deck)
+  # end
+
+  # My solution (tutorial solution is the same)
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 
   def deal([head | tail]) do
@@ -39,10 +50,6 @@ defmodule Cards do
     deal(deck, hand, number_of_cards_to_deal - 1)
   end
 
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
-  end
-
   def save_deck(deck, filename) do
     {:ok, file} = File.open filename, [:write]
     IO.binwrite(file, Enum.join(deck, ","))
@@ -52,5 +59,26 @@ defmodule Cards do
   def load_deck(filename) do
     {:ok, data} = File.read filename
     String.split(data, ",")
+  end
+
+  # My solution (does not work)
+  # def create_deck_advanced do
+  #   values = ["Ace", "Two", "Three", "Four", "Five"]
+  #   suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  #   deck = []
+  #
+  #   Enum.each(values, fn(value) -> Enum.each(suits, fn(suit) -> deck = deck ++ ["#{value} of #{suit}"] end) end)
+  #   deck
+  # end
+
+  def create_deck_advanced do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      for value <- values do
+        "#{value} of #{suit}"
+      end
+    end
   end
 end
